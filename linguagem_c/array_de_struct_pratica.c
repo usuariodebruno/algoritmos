9c92e82d239b7be33967e79355043788e75b173b
Bruno Almeida <usuariodebruno@gmail.com>
2023-01-03 12:11:16 -0300

    modified:   linguagem_c/array_de_struct_pratica.c 	modified:   linguagem_c/array_de_struct_pratica.exe

--- linguagem_c/array_de_struct_pratica.c
+++ linguagem_c/array_de_struct_pratica.c
@@ -8,6 +8,7 @@ struct aluno{
 
 
 // UM ARRAY É UM PONTEIRO, ENTÃO PARA LER DADOS, PODEMOS RECEBER O PONTEIRO 
+// Nesse caso: alunos[] = *alunos
 void ler_dados(int qtd, struct aluno *alunos){ 
     int i, j;
     for ( i = 0; i < qtd ; ++i){
@@ -34,13 +35,34 @@ void mostrar_dados(int qtd, struct aluno *alunos){
     }    
 }
 
+int media_aluno(struct aluno al){
+    int i, soma_notas=0, soma_pesos=0;
+    for (i = 0; i < al.qtd_notas; i++) {
+        soma_notas += al.notas[i]*al.pesos[i];
+        soma_pesos += al.pesos[i];
+    }
+    return soma_notas/soma_pesos;
+}
+
+int calcular_media_turma(int qtd,  struct aluno alunos[]){ //RECEBENDO AGORA COMO ARRAY
+    int i, soma_notas=0;
+    for (i = 0; i < qtd; i++){
+        soma_notas+=media_aluno(alunos[i]);
+    }
+    return soma_notas/qtd;
+}
+
 int main(){
     int qtd_alunos;
     printf("Quantidade de aluno\n");
     scanf("%d", &qtd_alunos);
+
+    // struct de array 
     struct aluno alunos[qtd_alunos]; // struct tipo nome_variavel[quantidade]
+
     ler_dados(qtd_alunos, alunos); // alunos passará o endereço de memoria(ponteiro de alunos)
     mostrar_dados(qtd_alunos, alunos);
-    
+    printf("Media da turma %d\n",calcular_media_turma(qtd_alunos,alunos));
+
     return 0;
 }
