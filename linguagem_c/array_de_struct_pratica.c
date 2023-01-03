8ae3be16997d5338e0c511f5cd1b973f0f139519
Bruno Almeida <usuariodebruno@gmail.com>
2023-01-03 11:46:07 -0300

    modified:   linguagem_c/array_de_struct_pratica.c 	modified:   linguagem_c/array_de_struct_pratica.exe

--- linguagem_c/array_de_struct_pratica.c
+++ linguagem_c/array_de_struct_pratica.c
@@ -6,39 +6,37 @@ struct aluno{
     int qtd_notas;
 };
 
-void ler_dados(int qtd, struct aluno *alunos){
+
+// UM ARRAY É UM PONTEIRO, ENTÃO PARA LER DADOS, PODEMOS RECEBER O PONTEIRO 
+void ler_dados(int qtd, struct aluno *alunos){ 
     int i, j;
     for ( i = 0; i < qtd ; ++i){
-         printf("Matricula de aluno");
+        printf("Matricula de aluno\n");
         scanf("%lld", &alunos[i].matriculas);
-         printf("Quantidade de notas");
+        printf("Quantidade de notas\n");
         scanf("%d",&alunos[i].qtd_notas);
         for (j = 0; j < alunos[i].qtd_notas; ++j){
-             printf("Nota | Peso");
+            printf("Nota | Peso\n");
             scanf ("%d %d", &alunos[i].notas[j], &alunos[j].pesos[j]);
-        }
-        
-    }
-    
+        }        
+    }    
 }
 
 void mostrar_dados(int qtd, struct aluno *alunos){
     int i, j;
-    for ( i = 0; i < qtd ; ++i){
-        printf("%lld", &alunos[i].matriculas); //não quebra linha 
-        printf("%d = { ",&alunos[i].qtd_notas);
+    for ( i = 0; i < qtd; ++i){
+        printf("%lld", alunos[i].matriculas); //não quebra linha 
+        printf("%d = { ",alunos[i].qtd_notas);
         for (j = 0; j < alunos[i].qtd_notas; ++j){
-            printf ("%d %d", &alunos[i].notas[j], &alunos[j].pesos[j]);
+            printf ("%d %d | ",alunos[i].notas[j],alunos[j].pesos[j]);
         }
-    printf(" } \n");
-        
-    }
-    
+    printf(" }\n");
+    }    
 }
 
 int main(){
     int qtd_alunos;
-    printf("Quantidade de aluno");
+    printf("Quantidade de aluno\n");
     scanf("%d", &qtd_alunos);
     struct aluno alunos[qtd_alunos]; // struct tipo nome_variavel[quantidade]
     ler_dados(qtd_alunos, alunos); // alunos passará o endereço de memoria(ponteiro de alunos)
