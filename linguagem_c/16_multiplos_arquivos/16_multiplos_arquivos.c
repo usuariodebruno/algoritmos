097e658c2234898bc72168a50219960f1af40280
Bruno Almeida <usuariodebruno@gmail.com>
2023-01-03 21:04:32 -0300

    update

---
+++ linguagem_c/16_multiplos_arquivos/16_multiplos_arquivos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+
+
+int main(){
+    clock_t inicio, fim;
+    int i, erro, *area1, qtd_elemntos=10, qtd_elemntos_usados=0; // ponteiro + qua
+    area1 = (int*) malloc(sizeof(int)*qtd_elemntos);
+    inicio = clock();
+    if (/* condition */)
+    {
+        /* code */
+    }
+    
+    return 0;
+} 
